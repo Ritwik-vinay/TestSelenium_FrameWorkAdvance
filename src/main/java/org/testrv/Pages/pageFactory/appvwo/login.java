3d99de7ce0244986571bc3413fb3d88a899526fe
ritwik vinay <ritwikvinay38@gmail.com>
2025-05-27 22:50:12 +0530

    Initial Commit

--- src/main/java/org/testrv/Pages/pageFactory/appvwo/login.java
+++ src/main/java/org/testrv/Pages/pageFactory/appvwo/login.java
@@ -1,4 +1,18 @@
 package org.testrv.Pages.pageFactory.appvwo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class login {
+    WebDriver driver;
+    login(WebDriver driver){
+        this.driver= driver;
+    }
+
+    //Page Locators
+    private  By emailid= By.id("login-username");
+    private By password= By.id("login-password");
+
+    //Step 2- Page Actions
+
 }
--- src/main/java/org/testrv/util/waitHelpers.java
+++ src/main/java/org/testrv/util/waitHelpers.java
@@ -1,4 +1,58 @@
 package org.testrv.util;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class waitHelpers {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public waitHelpers(WebDriver driver, int timeoutInSeconds) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
+    }
+
+    public WebElement waitForElementToBeVisible(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public WebElement waitForElementToBeClickable(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public boolean waitForElementToBeInvisible(By locator) {
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+
+    public boolean waitForUrlToContain(String partialUrl) {
+        return wait.until(ExpectedConditions.urlContains(partialUrl));
+    }
+
+    public boolean waitForTitleToBe(String expectedTitle) {
+        return wait.until(ExpectedConditions.titleIs(expectedTitle));
+    }
+
+    public boolean waitForElementTextToBe(By locator, String expectedText) {
+        return wait.until(ExpectedConditions.textToBe(locator, expectedText));
+    }
+
+    public Alert waitForAlertToBePresent() {
+        return wait.until(ExpectedConditions.alertIsPresent());
+    }
+
+    public boolean waitForElementToBeSelected(By locator) {
+        return wait.until(ExpectedConditions.elementToBeSelected(locator));
+    }
+
+    public WebElement waitForPresenceOfElement(By locator) {
+        return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    public boolean waitForJavaScriptToLoadCompletely() {
+        return wait.until(driver -> ((JavascriptExecutor) driver)
+                .executeScript("return document.readyState").equals("complete"));
+    }
 }
